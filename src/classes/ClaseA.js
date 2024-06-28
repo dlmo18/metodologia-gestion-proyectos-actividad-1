bc80202e647fa6172ba6de870f792c3a4beefa98
David Molina <dlmo18@gmail.com>
2024-06-28 11:00:30 -0500

    se agrega la estructura de archivos inicial y se crea la ClaseA con el atributo foo_bar

---
+++ src/classes/ClaseA.js
@@ -0,0 +1,11 @@
+let classA = class ClaseA {
+
+    foo_bar = "Foo Bar";
+
+    constructor(){
+        console.log('ClaseA Inicio', this.foo_bar)
+    }
+
+}
+
+export default classA;
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import ClaseA from './classes/ClaseA.js';
+
+let main = {
+    'ini': function(document) {
+        console.log('Inicio')
+
+        let claseA = new ClaseA();
+
+    }
+}
+
+main.ini(document);
