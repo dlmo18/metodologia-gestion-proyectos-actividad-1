240576b373a9f68f5fa00c560e21f34b90885d24
David Molina <dlmo18@gmail.com>
2024-06-28 11:32:59 -0500

    agregada ClaseC

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import ClaseA from './classes/ClaseA.js';
+import ClaseC from './classes/ClaseC.js';
+
 
 let main = {
     'ini': function(document) {
         console.log('Inicio')
 
         let claseA = new ClaseA();
+        let claseC = new ClaseC();
 
     }
 }
