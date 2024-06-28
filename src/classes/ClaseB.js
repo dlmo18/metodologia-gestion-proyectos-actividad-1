0bfc660defa851c1c6359bfbcd7c5c19f76c1418
David Molina <dlmo18@gmail.com>
2024-06-28 11:26:44 -0500

    agregada ClaseB

---
+++ src/classes/ClaseB.js
@@ -0,0 +1,11 @@
+let classA = class ClaseB {
+
+    foo_bar = "Foo Bar B";
+
+    constructor(){
+        console.log('ClaseB Inicio', this.foo_bar)
+    }
+
+}
+
+export default classA;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import ClaseA from './classes/ClaseA.js';
+import ClaseB from './classes/ClaseB.js';
 
 let main = {
     'ini': function(document) {
         console.log('Inicio')
 
         let claseA = new ClaseA();
-
+        let claseB = new ClaseB();
     }
 }
 
